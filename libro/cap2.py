79dc8c1584b9fdf7ea427cd87d0599fe172837de
Sergio <sergiorazza@hotmail.com>
2025-05-17 17:09:22 -0300

    Fix formatting of code sections in cap2.py

--- libro/cap2.py
+++ libro/cap2.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 c = (-9.5, 11.75)
 type(c)
 # <class 'tuple'>
@@ -6,4 +6,21 @@ type(c)
 c
 # %%
 c[0]
+
+
+# %%
+i = 0.1
+def D(c1):
+    return c1 / (1 + i)
+
+# %%
+D(100)
+# %%
+import numpy as np
+# %%
+a = np.array([1, 2, 3])
+# %%
+type(a)
+# %%
+a * 2 + 5
 # %%
