89514910e306cfa56ff74d9e9c4f1b511e4c085d
Sergio <sergiorazza@hotmail.com>
2025-06-08 19:40:48 -0300

    pruebas

--- libro/cap2.py
+++ libro/cap2.py
@@ -24,3 +24,17 @@ type(a)
 # %%
 a * 2 + 5
 # %%
+
+import numpy as np
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+print(arr1 * arr2) # Salida: [ 4 10 18] (1*4, 2*5, 3*6)
+
+matriz1 = np.array([[1, 2],
+                    [3, 4]])
+matriz2 = np.array([[5, 6],
+                    [7, 8]])
+print(matriz1 * matriz2)
+# %%
+matriz1 @ matriz2
+# %%
